85e65f01d199c738cbcb63aaf456e11695edce64
Pete Marchetto <petmar@gmail.com>
2018-04-05 07:54:05 -0500

    Update sucker.sh

--- sucker.sh
+++ sucker.sh
@@ -4,7 +4,7 @@ gpio mode 21 out
 gpio write 20 0
 gpio write 21 1
 today=`date +%Y-%m-%d.%H:%M:%S`
-mkdir /mnt/target/"$today"
-cp /mnt/usb/* /mnt/target/"$today"
+mkdir /mnt/target/$today
+cp /mnt/usb/* /mnt/target/$today
 gpio write 20 1
 gpio write 21 0
